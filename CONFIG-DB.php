54ee911b325462ee41df8eaa33cf95e5121b2236
tamarakhalid04-tech <tamarakhalid04@gmail.com>
2025-11-28 01:00:08 +0300

    CONFIG-DB.php

--- CONFIG-DB.php
+++ CONFIG-DB.php
@@ -5,12 +5,4 @@
     define("DBNAME", "faculty_load_db");
 
 
-$conn = new mysqli(DBHOST , DBUSER , DBPWD , DBNAME);
-if($conn-> connect_error){
-
-
-die("Connection failed : " . $conn->connect_error);
-
-}
-
 ?>
